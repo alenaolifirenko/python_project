ee286e0e74a082987284cd1297c5fd239ceef96d
BlackSamorez <robominerminecraf@gmail.com>
2019-11-15 12:25:05 +0300

    несколько ракет и притягивающих лет

--- project.py
+++ project.py
@@ -30,10 +30,10 @@ class rocket():
 		self.x0 = event.x #точка на экране
 		self.y0 = event.y #точка на экране
 	def accel(self): #ускорение ракеты
-		global force, fd
-		if self.power:
-			x0 = self.x0 - self.x #наведение ускорения
-			y0 = self.y0 - self.y #наведение ускорения
+		global force, fd, power, screenx, screeny
+		if power:
+			x0 = screenx - self.x #наведение ускорения
+			y0 = screeny - self.y #наведение ускорения
 			self.ax = x0 / ((x0 ** 2 + y0 ** 2)) ** (1/2) / (self.fuel + 0.2) + force[0]
 			self.ay = y0 / ((x0 ** 2 + y0 ** 2)) ** (1/2) / (self.fuel + 0.2) + force[1]
 		else:
@@ -41,9 +41,10 @@ class rocket():
 			self.ay = force[1]
 
 	def forced(self):
-		global force, fd
+		global force, fd, n
 		force = [0, 0]
-		fd.force(self.x, self.y)
+		for i in range(n):
+			fd[i].force(self.x, self.y)
 
 	def move(self):
 		global force, fd
@@ -53,6 +54,9 @@ class rocket():
 		self.y += (self.vy + self.ay / 2) #Перемещение
 		self.vx += self.ax
 		self.vy += self.ay
+		if self.vx **2 + self.vy ** 2 > 1000:
+			self.vx = self.vx * 0.8
+			self.vy = self.vy * 0.8
 		if self.x < 0 or self.x > 800:
 			self.vx = - self.vx
 		if self.y < 0 or self.y > 600:
@@ -66,26 +70,51 @@ class rocket():
 		self.power = 0
 	
 class field(): #недописано
-	def __init__(self, x = 450, y = 300, I = -1000):
+	def __init__(self, x = 400 , y = 400, I = 0):
 		self.x = x
 		self.y = y
 		self.I = I
 		self.r = 10
-		self.id = canv.create_oval(self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r, fill='red')
+		self.id = canv.create_oval(self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r, fill='blue')
 	def force(self, x, y):
 		global force
 		force[0] += self.I * (x - self.x) / ((x - self.x) ** 2 + (y - self.y) ** 2) ** (3 / 2)
 		force[1] += self.I * (y - self.y) / ((x - self.x) ** 2 + (y - self.y) ** 2) ** (3 / 2)
+	def set_coords(self):
+		canv.coords(self.id, self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r)
+
+def positioning(event):
+	global screenx, screeny
+	screenx = event.x
+	screeny = event.y
+
+def power_start(event):
+	global power
+	power = 1
+
+def power_end(event):
+	global power
+	power = 0
 
 def new_game(event = ''):
-	global r1, force, fd
-	force = [0, 0] 
-	r1 = rocket()
-	fd = field()
-	canv.bind('<Button-1>', r1.power_start) #тяга при нажатии
-	r1.move()
-	canv.bind('<ButtonRelease-1>', r1.power_end) #нет тяги при отпускании
-	canv.bind('<Motion>', r1.positioning) #наведение мышкой
+	global r1, force, fd, n, k, power, screenx, screeny
+	force = [0, 0]
+	power = 0 
+	n = 5
+	k = 5
+	r = [rocket(rnd(100, 500)), rocket(rnd(100, 500)), rocket(rnd(100, 500)), rocket(rnd(100, 500)), rocket(rnd(100, 500))]
+	fd = [field(rnd(100, 500), rnd(100, 500)), field(rnd(100, 500), rnd(100, 500)), field(rnd(100, 500), rnd(100, 500)), field(rnd(100, 500), rnd(100, 500)), field(rnd(100, 500), rnd(100, 500))]
+	
+	canv.bind('<Button-1>', power_start)
+	canv.bind('<ButtonRelease-1>', power_end)
+	canv.bind('<Motion>', positioning)
+
+
+	for i in range(k):	
+		#canv.bind('<Button-1>', r[i].power_start)
+		r[i].move()
+		#canv.bind('<ButtonRelease-1>', r[i].power_end) 
+		#canv.bind('<Motion>', r[i].positioning) 
 	#root.after(10,new_game)
 
 
