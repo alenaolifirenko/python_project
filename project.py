c26515fb3b40b7b46a55c8d158d6fe7e9aae96ee
BlackSamorez <robominerminecraf@gmail.com>
2019-11-15 14:17:37 +0300

    Ускорение времени

--- project.py
+++ project.py
@@ -1,12 +1,18 @@
 from random import randrange as rnd,choice
-from tkinter import*
+import tkinter as tk
 import math
 import time
-root=Tk()
-fr=Frame(root)
+root = tk.Tk()
+fr = tk.Frame(root)
 root.geometry('800x600')
-canv=Canvas(root,bg='black')
-canv.pack(fill=BOTH,expand=1)
+canv = tk.Canvas(root,bg='black')
+canv.pack(fill = tk.BOTH,expand = 1)
+
+dvdt = tk.DoubleVar()
+
+scale = tk.Scale(root, variable = dvdt, orient = tk.HORIZONTAL)
+scale.pack(side = tk.LEFT)
+
 
 #Объект, которым мы управляем, ракета
 class rocket():
@@ -26,34 +32,34 @@ class rocket():
 		self.id = canv.create_oval(self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r, fill='red')
 	def set_coords(self):
 		canv.coords(self.id, self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r)
-	def positioning(self, event):
-		self.x0 = event.x #точка на экране
-		self.y0 = event.y #точка на экране
-	def accel(self): #ускорение ракеты
+	def positioning(self, event): #not used yet
+		self.x0 = event.x
+		self.y0 = event.y 
+	def accel(self): #rocket acceleration
 		global force, fd, power, screenx, screeny
 		if power:
-			x0 = screenx - self.x #наведение ускорения
-			y0 = screeny - self.y #наведение ускорения
+			x0 = screenx - self.x #acccel course
+			y0 = screeny - self.y #acccel course
 			self.ax = x0 / ((x0 ** 2 + y0 ** 2)) ** (1/2) / (self.fuel + 0.2) + force[0]
 			self.ay = y0 / ((x0 ** 2 + y0 ** 2)) ** (1/2) / (self.fuel + 0.2) + force[1]
 		else:
 			self.ax = force[0]
 			self.ay = force[1]
 
-	def forced(self):
+	def forced(self): #force commited on rocket
 		global force, fd, n
 		force = [0, 0]
 		for i in range(n):
 			fd[i].force(self.x, self.y)
 
 	def move(self):
-		global force, fd
+		global force, fd, dt
 		self.forced()
 		self.accel()
-		self.x += (self.vx + self.ax / 2) #Перемещение
-		self.y += (self.vy + self.ay / 2) #Перемещение
-		self.vx += self.ax
-		self.vy += self.ay
+		self.x += (self.vx * dt + self.ax * dt ** 2 / 2) #Перемещение
+		self.y += (self.vy * dt + self.ay * dt ** 2 / 2) #Перемещение
+		self.vx += self.ax * dt
+		self.vy += self.ay * dt
 		if self.vx **2 + self.vy ** 2 > 1000:
 			self.vx = self.vx * 0.8
 			self.vy = self.vy * 0.8
@@ -62,6 +68,7 @@ class rocket():
 		if self.y < 0 or self.y > 600:
 			self.vy = - self.vy
 		self.set_coords()
+		print(dt)
 		canv.update()
 		root.after(50, self.move) #50мс - частота обновления
 	def power_start(self, event):
@@ -70,13 +77,13 @@ class rocket():
 		self.power = 0
 	
 class field(): #недописано
-	def __init__(self, x = 400 , y = 400, I = 0):
+	def __init__(self, x = 400 , y = 400, I = -1000):
 		self.x = x
 		self.y = y
 		self.I = I
 		self.r = 10
 		self.id = canv.create_oval(self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r, fill='blue')
-	def force(self, x, y):
+	def force(self, x, y): #force in position x y
 		global force
 		force[0] += self.I * (x - self.x) / ((x - self.x) ** 2 + (y - self.y) ** 2) ** (3 / 2)
 		force[1] += self.I * (y - self.y) / ((x - self.x) ** 2 + (y - self.y) ** 2) ** (3 / 2)
@@ -85,23 +92,29 @@ class field(): #недописано
 
 def positioning(event):
 	global screenx, screeny
-	screenx = event.x
-	screeny = event.y
+	screenx = event.x   #mouse position x
+	screeny = event.y   #mouse position y
 
 def power_start(event):
 	global power
-	power = 1
+	power = 1 #power start trigger
 
 def power_end(event):
 	global power
-	power = 0
+	power = 0 #power end trigger
+
+def time_speed_read():
+	global dvdt, dt
+	dt = dvdt.get() / 100
+	root.after(100, time_speed_read)
 
 def new_game(event = ''):
-	global r1, force, fd, n, k, power, screenx, screeny
+	global r1, force, fd, n, k, power, screenx, screeny, dt, dvdt
 	force = [0, 0]
-	power = 0 
-	n = 5
-	k = 5
+	power = 0
+	time_speed_read() 
+	n = 5 # number of fields
+	k = 5 # number of rockets
 	r = [rocket(rnd(100, 500)), rocket(rnd(100, 500)), rocket(rnd(100, 500)), rocket(rnd(100, 500)), rocket(rnd(100, 500))]
 	fd = [field(rnd(100, 500), rnd(100, 500)), field(rnd(100, 500), rnd(100, 500)), field(rnd(100, 500), rnd(100, 500)), field(rnd(100, 500), rnd(100, 500)), field(rnd(100, 500), rnd(100, 500))]
 	
@@ -111,13 +124,10 @@ def new_game(event = ''):
 
 
 	for i in range(k):	
-		#canv.bind('<Button-1>', r[i].power_start)
-		r[i].move()
-		#canv.bind('<ButtonRelease-1>', r[i].power_end) 
-		#canv.bind('<Motion>', r[i].positioning) 
+		r[i].move() #rocket launch
 	#root.after(10,new_game)
 
 
 new_game()
 
-mainloop()
+tk.mainloop()
