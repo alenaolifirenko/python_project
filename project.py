ab7254200d78643a224f40be60d50e059cb8143a
BlackSamorez <robominerminecraf@gmail.com>
2019-11-15 11:26:20 +0300

    Прикрутил поля

--- project.py
+++ project.py
@@ -27,10 +27,10 @@ class rocket():
 	def set_coords(self):
 		canv.coords(self.id, self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r)
 	def positioning(self, event):
-		self.x0 = event.x #наведение ускорения
-		self.y0 = event.y #наведение ускорения
-	def forced(self): #ускорение ракеты
-		global force
+		self.x0 = event.x #точка на экране
+		self.y0 = event.y #точка на экране
+	def accel(self): #ускорение ракеты
+		global force, fd
 		if self.power:
 			x0 = self.x0 - self.x #наведение ускорения
 			y0 = self.y0 - self.y #наведение ускорения
@@ -39,8 +39,16 @@ class rocket():
 		else:
 			self.ax = force[0]
 			self.ay = force[1]
+
+	def forced(self):
+		global force, fd
+		force = [0, 0]
+		fd.force(self.x, self.y)
+
 	def move(self):
+		global force, fd
 		self.forced()
+		self.accel()
 		self.x += (self.vx + self.ax / 2) #Перемещение
 		self.y += (self.vy + self.ay / 2) #Перемещение
 		self.vx += self.ax
@@ -58,7 +66,7 @@ class rocket():
 		self.power = 0
 	
 class field(): #недописано
-	def __init__(self, x = 0, y = 0, I = 1):
+	def __init__(self, x = 450, y = 300, I = -1000):
 		self.x = x
 		self.y = y
 		self.I = I
@@ -66,14 +74,14 @@ class field(): #недописано
 		self.id = canv.create_oval(self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r, fill='red')
 	def force(self, x, y):
 		global force
-		force[0] += I * x / (x ** 2 + y ** 2) ** (3 / 2)
-		force[1] += I * y / (x ** 2 + y ** 2) ** (3 / 2)
-		return force
+		force[0] += self.I * (x - self.x) / ((x - self.x) ** 2 + (y - self.y) ** 2) ** (3 / 2)
+		force[1] += self.I * (y - self.y) / ((x - self.x) ** 2 + (y - self.y) ** 2) ** (3 / 2)
 
 def new_game(event = ''):
-	global r1, force
+	global r1, force, fd
 	force = [0, 0] 
 	r1 = rocket()
+	fd = field()
 	canv.bind('<Button-1>', r1.power_start) #тяга при нажатии
 	r1.move()
 	canv.bind('<ButtonRelease-1>', r1.power_end) #нет тяги при отпускании
